ff5dc9a10579b2c9c81b277753449935dd144bb7
Rehanuddin <s.rehanuddin@gmail.com>
2015-09-12 03:51:33 +0500

    Firebase Added - V4

--- V4 - With Angular2 & Firebase/client/public/js/app.js
+++ V4 - With Angular2 & Firebase/client/public/js/app.js
@@ -69,51 +69,6 @@ var MyAppComponent = (function () {
                 _self.rooms.push(room.name);
             }
         });
-        /*
-        
-        _self.socket.on('connect', function(){
-              // call the server-side function 'adduser' and send one parameter (value of prompt)
-              _self.name = prompt("What's your name?");
-              _self.socket.emit('adduser', _self.name);
-          });
-    
-          // listener, whenever the server emits 'updatechat', this updates the chat body
-          _self.socket.on('updatechat', function (username, data) {
-              
-              _self.conversation.push({
-                user : username,
-                message : data
-              });
-              //_self.conversation.push('<b>' + username + ':</b> ' + data + '<br>');
-              
-              console.log(_self.conversation);
-              
-              var elem = document.getElementById('conversation');
-              elem.scrollTop = elem.scrollHeight;
-          });
-    
-          // listener, whenever the server emits 'updaterooms', this updates the room the client is in
-          _self.socket.on('updaterooms', function(rooms, current_room) {
-              _self.rooms = rooms;
-              _self.current_room = current_room;
-          });
-    
-          _self.socket.on('appendnewroom', function(username, newroom) {
-              _self.rooms.push(newroom);
-          });
-    
-          // listener, whenever the server emits 'updateusers', this updates the username list
-          _self.socket.on('updateusers', function(data) {
-              _self.users = [];
-              for(var key in data){
-                _self.users.push(key);
-              }
-              
-              console.log("users");
-              console.log(data);
-          });
-          
-          */
     }
     MyAppComponent.prototype.getMessageId = function (snapshot) {
         return snapshot.key().replace(/[^a-z0-9\-\_]/gi, '');
@@ -123,7 +78,7 @@ var MyAppComponent = (function () {
         this.conversation = [];
         var _self = this;
         //this.myRoomRef = this.dataRef.child("conversation").child(room);
-        this.myRoomRef = new serviceFirebase_1.FirebaseService.Firebase('https://angular2chatapp.firebaseio.com').child("conversation").child(room);
+        this.myRoomRef = new serviceFirebase_1.FirebaseService.Firebase(serviceFirebase_1.FirebaseService.FirebaseURL).child("conversation").child(room);
         this.myRoomRef.on("child_added", function (snapshot) {
             var obj = snapshot.val();
             _self.conversation.push({ user: obj.user, message: obj.message });
